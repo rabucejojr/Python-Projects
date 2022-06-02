808326a55ff7ed534e7d1dd7e48c31f067e17c1a
rabucejo <rogerhapay@gmail.com>
2022-06-02 08:14:04 +0800

    modified clock function

--- CLOCK/clock.py
+++ CLOCK/clock.py
@@ -5,24 +5,14 @@ from time import strftime
 root = Tk()
 root.title("Digital Clock using Python Tkinter")
 
+clock_label = Label(root, font=('sans', 60), fg='black')
+
 def clock():
-    tick = strftime("%H:%M:%S")
+    tick = strftime("%H:%M:%S %p")
     clock_label.config(text=tick)
     clock_label.after(1000, clock)
-    
-#screen width and height
-width = root.winfo_screenwidth()
-height = root.winfo_screenheight()
-#set the window size
-win_width = 500
-win_height = 200    
-#frame size
-center_x = int(width/2 - win_width/2)
-center_y = int(height/2 - win_height/2)
+    clock_label.pack(anchor=CENTER)
 
-clock_label = Label(root,font=('sans', 40), fg='black')
-clock_label.pack(anchor='center')
-
-root.geometry(f"{win_width}x{win_height}+{center_x}+{center_y}")
+root.geometry()
 clock()
 mainloop()
